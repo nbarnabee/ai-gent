70c48b73250d8784b2c0836d1e6e659b71afc515
nbarnabee <nicole.barnabee@gmail.com>
2025-06-22 13:25:13 +0200

    Build schemas

---
+++ schemas.py
@@ -0,0 +1,78 @@
+# Defining the functions for Gemini
+
+from google import genai
+from google.genai import types
+
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Reads the content of the specified file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file that will be read, relative to the working directory.  This value is required.",
+            ),
+        },
+    ),
+)
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory.  If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Executes the specified python file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file that will be executed, relative to the working directory.  This value is required.",
+            ),
+        },
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes the given string to the specified file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+               type=types.Type.STRING,
+               description="The path to the file that will be written to or overwritten, relative to the working directory.  This value is required.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The string that will be written to the file.  This will be included in the prompt.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_file_content,
+        schema_get_files_info,
+        schema_run_python_file,
+        schema_write_file,
+    ]
+)
